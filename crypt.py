898ddb677fd7fecba0451dce72312108327c6948
QwantisheKU <34191646+QwantisheKU@users.noreply.github.com>
2021-10-15 00:16:56 +0300

    added hangman game + test

--- crypt.py
+++ crypt.py
@@ -21,3 +21,50 @@ class Decoder:
         for k in sorted(key):
             tmp[k], msg = msg[:group[k]], msg[group[k]:]
         return("".join("".join(x) for x in zip_longest(*(tmp[k] for k in key), fillvalue="")))
+
+class Hangman:
+    def hangman():
+        word = 'ЛАДЬЯ'
+        flag = False
+        health = 10
+        test=False
+        used_letters=""
+        win_word=[]
+        len_word=len(word)
+
+        for i in range(len(word)):
+            win_word+="_"
+        
+        while len_word != 0 and health != 0:
+            test = False
+
+            while True:
+                letter = input("\nВаша буква: ")
+                if letter in used_letters or len(letter)>1:
+                    print("\nНе больше одного символа, попробуйте снова!")
+                else:
+                    used_letters+=letter
+                    break
+            count=0
+            for i in word:
+                if letter in i:
+                    len_word -= 1
+                    test=True
+                    win_word[count]=letter
+                count+=1
+            if not test:
+                health -= 1
+                print("Неверно")
+            else:
+                print("Верно")
+                print(win_word)
+        
+            print("Ваше здоровье = ", health)
+        
+        if(len_word == 0):
+            
+            flag = True
+            print("\nИтоговый ключ: ", word.upper())
+        else:
+            print("\nВ попытках отказано, попробуйте снова!")
+        return 0
--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
-from crypt import Coder, Decoder
+from crypt import Coder, Decoder, Hangman
 
 def test_coder():
     assert Coder.coder("СУПЕР СЕКРЕТНОЕ СООБЩЕНИЕ", "ЛАДЬЯ") == "УСТСЕПЕНОНС Е ЩЕКООИРРЕБЕ"
 
 def test_decoder():
     assert Decoder.decoder("УСТСЕПЕНОНС Е ЩЕКООИРРЕБЕ", "ЛАДЬЯ") == "СУПЕР СЕКРЕТНОЕ СООБЩЕНИЕ"
+
+def test_hangman():
+    assert Hangman.hangman() == 0
