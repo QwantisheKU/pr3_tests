a26a9520de18bb6c38da2ab6e6af90609d287db1
QwantisheKU <34191646+QwantisheKU@users.noreply.github.com>
2021-10-15 00:05:17 +0300

    class decoder added + tests

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
-from crypt import Coder
+from crypt import Coder, Decoder
 
 def test_coder():
     assert Coder.coder("СУПЕР СЕКРЕТНОЕ СООБЩЕНИЕ", "ЛАДЬЯ") == "УСТСЕПЕНОНС Е ЩЕКООИРРЕБЕ"
+
+def test_decoder():
+    assert Decoder.decoder("УСТСЕПЕНОНС Е ЩЕКООИРРЕБЕ", "ЛАДЬЯ") == "СУПЕР СЕКРЕТНОЕ СООБЩЕНИЕ"
